5fb202d9f26ba65f00b92c8634c1eab5daff63e0
BigAru <arooran@indikon.ch>
2019-06-09 13:24:47 +0200

    add pgp signing skeleton

--- frontend/bundle.js
+++ frontend/bundle.js
@@ -1,6 +1,9 @@
-window.$ = window.jQuery = require('jquery');
-window.Bootstrap = require('bootstrap');
-window.speakeasy = require("speakeasy")
+window.$ = window.jQuery = require('jquery')
+window.Bootstrap = require('bootstrap')
+const speakeasy = require("speakeasy")
+const openpgp = require('openpgp')
+
+openpgp.initWorker({ path:'node_modules/openpgp/dist/openpgp.worker.js' })
 
 function loadIncomings(data){
     $('#incoming-body').html("")
@@ -16,18 +19,36 @@ function loadIncomings(data){
     }
 }
 
-
 const shared_totp = 'ig3t 76td vmok rffh iqgh 7kx5 kp2c ons6'
-var token = speakeasy.totp({
-    secret: shared_totp,
-    encoding: 'base32'
-  });
+const privkey = `-----BEGIN PGP PRIVATE KEY BLOCK-----
+-----END PGP PRIVATE KEY BLOCK-----`
+const passphrase = '---'
 
-const myHeaders = new Headers();
-myHeaders.append('Authorization', token);
+async function getSig(){
+    const privKeyObj = (await openpgp.key.readArmored(privkey)).keys[0];
+    await privKeyObj.decrypt(passphrase);
+
+    const options = {
+        message: openpgp.message.fromText('Hello, World!'),
+        privateKeys: [privKeyObj],
+        armor: true,
+        detached: true,
+    };
+    
+    let detachedSig = await openpgp.sign(options)
+    console.log(detachedSig.signature)
+}
+
+function getToken(){
+    getSig()
+    return speakeasy.totp({ secret: shared_totp, encoding: 'base32'})
+}
+
+const headers = new Headers();
+headers.append('Authorization', getToken());
 
 const URL = 'http://localhost'
-const getIncomings = new Request(URL + '/posts/', { method: 'GET', headers: myHeaders })
+const getIncomings = new Request(URL + '/posts/', { method: 'GET', headers: headers })
 
 fetch(getIncomings)
     .then(res => 
