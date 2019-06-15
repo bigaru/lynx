63643b1c43021d4e80cd2ed4b3e5efcc2a8e4ef5
BigAru <arooran@indikon.ch>
2019-06-15 19:45:13 +0200

    make mongoService more durable

--- backend/src/MongoService.ts
+++ backend/src/MongoService.ts
@@ -1,4 +1,4 @@
-import { MongoClient, Db, Collection } from 'mongodb'
+import { MongoClient, Collection } from 'mongodb'
 
 interface Memo{
     _id: string,
@@ -8,26 +8,47 @@ interface Memo{
 
 export default class MongoService{
     private client: MongoClient
-    private db: Db
-    private memos: Collection
+    private readonly dbName: string
+    private readonly dbUrl: string
 
     constructor(dbUrl: string, dbName: string){
-        this.client = new MongoClient(dbUrl, { useNewUrlParser: true, poolSize: 10 })
+        this.dbName = dbName
+        this.dbUrl = dbUrl
+        
+        this.connect()
+            .catch(_err => console.error("MongoDb driver can not connect"))
+    }
 
-        this.client.connect().then(client => {
-            this.db = client.db(dbName)
-            this.memos = this.db.collection("memos")
-        })
+    private connect = () => {
+        this.client = new MongoClient(this.dbUrl, { useNewUrlParser: true, poolSize: 5, autoReconnect: false })
+        this.client.on('close', () => console.error('MongoDb driver lost connection'));
+        return this.client.connect()
+    }
+    
+    private getCollection = (name: string): Promise<Collection> => {
+        if (this.client.isConnected()) {
+            return new Promise((resolve, _reject) => 
+                resolve(
+                    this.client.db(this.dbName).collection(name)
+                )
+            ) 
+        }
+        else {
+            return this.connect().then(newClient => 
+                newClient.db(this.dbName).collection(name)
+            )
+        }
     }
 
     addMemo = (document: object): Promise<{ ok: number, n: number }> => {
-        return this.memos.insertOne(document)
-                         .then(writeResult => writeResult.result)
+        return this.getCollection("memos")
+                    .then(col => col.insertOne(document))
+                    .then(writeResult => writeResult.result)
     }
 
     getMemos = (): Promise<Memo[]> => {
-        return this.memos.find({})
-                         .toArray()
+        return this.getCollection("memos")
+                    .then(col => col.find({}).toArray())
     }
 
     close = () => {
