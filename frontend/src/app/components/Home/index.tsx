25347b9cb15a26ad235115d13f863a77d8892494
BigAru <arooran@indikon.ch>
2019-08-17 16:09:29 +0200

    add dropZone

--- frontend/src/app/components/Home/index.tsx
+++ frontend/src/app/components/Home/index.tsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import * as styles from './style.css';
 import { Header } from '../index';
-import { Card, Button, TextArea, Intent, Dialog } from '@blueprintjs/core';
+import { Card, Button, TextArea, Intent, Dialog, Icon } from '@blueprintjs/core';
+import ReactDropzone from 'react-dropzone';
+import classNames from 'classnames';
 
 export enum CardType {
   MESSAGE,
@@ -13,6 +15,7 @@ interface State {
   type: CardType;
   isDialogOpen: boolean;
   messageText: string;
+  file: File | null;
 }
 
 export class Home extends Component<Props, State> {
@@ -21,7 +24,8 @@ export class Home extends Component<Props, State> {
   readonly state: State = {
     type: CardType.MESSAGE,
     isDialogOpen: false,
-    messageText: ''
+    messageText: '',
+    file: null
   };
 
   private debounce = (cb: () => void) => {
@@ -31,24 +35,92 @@ export class Home extends Component<Props, State> {
 
   private setCardType = (selectedType: CardType) => () => this.setState({ type: selectedType });
 
+  private renderMessageBody = () => (
+    <TextArea
+      className={styles.textMessage}
+      growVertically={true}
+      large={true}
+      intent={Intent.PRIMARY}
+      onChange={(event) => {
+        const val = event.target.value;
+        this.debounce(() => this.setState({ messageText: val }));
+      }}
+    />
+  );
+
+  private formatByteValue = (bytes: number, unitCounter: number): string => {
+    if (bytes > 1024) return this.formatByteValue(bytes / 1024, unitCounter + 1);
+
+    const roundedBytes = bytes.toFixed(2);
+
+    switch (unitCounter) {
+      case 1:
+        return `${roundedBytes} KB`;
+
+      case 2:
+        return `${roundedBytes} MB`;
+
+      case 3:
+        return `${roundedBytes} GB`;
+
+      case 4:
+        return `${roundedBytes} TB`;
+
+      default:
+        return `${roundedBytes} B`;
+    }
+  };
+
+  private renderFileBody = () => {
+    const dropContainerHover = (isDragActive: boolean) =>
+      isDragActive
+        ? classNames(styles.fileContainer, styles.dropContainer, styles.dropContainerActive)
+        : classNames(styles.fileContainer, styles.dropContainer);
+
+    const dropTextHover = (isDragActive: boolean) =>
+      isDragActive ? classNames(styles.dropText, styles.dropTextActive) : styles.dropText;
+
+    console.log(this.state.file);
+    const { file } = this.state;
+
+    return file ? (
+      <div className={styles.fileContainer}>
+        <Card className={styles.fileCard} elevation={2}>
+          <Icon icon="document" iconSize={42} intent={Intent.PRIMARY} />
+          <div className={styles.fileName}>
+            <strong>{file.name}</strong>
+            <span>{file.type}</span>
+          </div>
+
+          <strong className={styles.fileSize}>{this.formatByteValue(file.size, 0)}</strong>
+          <Button minimal onClick={() => this.setState({ file: null })}>
+            <Icon icon="cross" iconSize={32} />
+          </Button>
+        </Card>
+      </div>
+    ) : (
+      <ReactDropzone onDrop={(f) => this.setState({ file: f[0] })}>
+        {({ getRootProps, getInputProps, isDragActive }) => (
+          <div {...getRootProps()} className={dropContainerHover(isDragActive)}>
+            <input {...getInputProps()} />
+            <p className={dropTextHover(isDragActive)}>
+              {isDragActive ? 'Drop the file here' : 'Drag & drop a file'}
+            </p>
+            {isDragActive ? null : <Button>Choose a file</Button>}
+          </div>
+        )}
+      </ReactDropzone>
+    );
+  };
+
   render = () => {
     const { type, isDialogOpen } = this.state;
 
     return (
       <Card className={styles.card}>
         <Header type={type} onClick={this.setCardType} />
-        {type === CardType.MESSAGE ? (
-          <TextArea
-            className={styles.textMessage}
-            growVertically={true}
-            large={true}
-            intent={Intent.PRIMARY}
-            onChange={(event) => {
-              const val = event.target.value;
-              this.debounce(() => this.setState({ messageText: val }));
-            }}
-          />
-        ) : null}
+
+        {type === CardType.MESSAGE ? this.renderMessageBody() : this.renderFileBody()}
 
         <Button
           intent={Intent.PRIMARY}
